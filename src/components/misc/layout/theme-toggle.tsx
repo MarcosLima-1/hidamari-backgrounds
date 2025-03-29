b2b93ea33bc3a65bc95d7b70fa9e6b820614f87b
Markin <Markin-work@protonmail.com>
2025-03-29 14:47:44 -0300

    feat: square background

--- src/components/misc/layout/theme-toggle.tsx
+++ src/components/misc/layout/theme-toggle.tsx
@@ -1,8 +1,6 @@
-'use client'
-
 import { Moon, Sun } from 'lucide-react'
-import { useTheme } from 'next-themes'
 
+import { useTheme } from '@/components/misc/layout/theme-provider'
 import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
@@ -11,15 +9,15 @@ import {
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 
-export function ThemeToggle() {
+export function ModeToggle() {
   const { setTheme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='outline' size='icon'>
-          <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 text-black transition-all dark:-rotate-90 dark:scale-0' />
-          <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 text-white transition-all dark:rotate-0 dark:scale-100' />
+          <Sun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
+          <Moon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
