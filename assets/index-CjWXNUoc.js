c244bb5c928b57587f97512b992c1741b5a1f9e1
MarcosLima-1 <MarcosLima-1@users.noreply.github.com>
2025-03-30 11:30:56 +0000

    deploy: 3f1b0cb91ee06150c336896a150b2b9ad00b1220

--- assets/index-CjWXNUoc.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CHEMIjCK.js";const e=function(){return o.jsx("div",{children:"Isso e um test ta bom"})};export{e as component};
---
+++ assets/index-DnD4_ee_.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DS-5ozUU.js";const e=function(){return o.jsx("div",{children:"Isso e um test ta bom"})};export{e as component};
